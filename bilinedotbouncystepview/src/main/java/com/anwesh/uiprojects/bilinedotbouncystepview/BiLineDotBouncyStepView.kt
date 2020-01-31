892ce282758de9fc797e7ab7d92336ca078a9c90
Anwesh43 <anweshthecool0@gmail.com>
2020-01-31 11:07:57 +0530

    adding BLDSNode class

--- bilinedotbouncystepview/src/main/java/com/anwesh/uiprojects/bilinedotbouncystepview/BiLineDotBouncyStepView.kt
+++ bilinedotbouncystepview/src/main/java/com/anwesh/uiprojects/bilinedotbouncystepview/BiLineDotBouncyStepView.kt
@@ -121,4 +121,47 @@ class BiLineDotBouncyStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class BLDSNode(var i : Int, val state : State = State()) {
+
+        private var next : BLDSNode? = null
+        private var prev : BLDSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = BLDSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawBLDSNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : BLDSNode {
+            var curr : BLDSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
+
 }
