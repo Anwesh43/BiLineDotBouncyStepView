8ef0b52a16ea35cf687981aacf455232e75b5832
Anwesh43 <anweshthecool0@gmail.com>
2020-01-31 11:14:54 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedbilinedotbouncystepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedbilinedotbouncystepview/MainActivity.kt
@@ -2,11 +2,12 @@ package com.anwesh.uiprojects.linkedbilinedotbouncystepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.anwesh.uiprojects.bilinedotbouncystepview.BiLineDotBouncyStepView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        BiLineDotBouncyStepView.create(this)
     }
 }
