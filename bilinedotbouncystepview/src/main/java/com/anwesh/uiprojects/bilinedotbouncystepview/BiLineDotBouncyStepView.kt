f0ad3b868dd64c29f57c48c0401636e9a39a7fc0
Anwesh43 <anweshthecool0@gmail.com>
2020-01-31 11:01:14 +0530

    adding BiLineDotBouncyView class

--- bilinedotbouncystepview/src/main/java/com/anwesh/uiprojects/bilinedotbouncystepview/BiLineDotBouncyStepView.kt
+++ bilinedotbouncystepview/src/main/java/com/anwesh/uiprojects/bilinedotbouncystepview/BiLineDotBouncyStepView.kt
@@ -56,3 +56,21 @@ fun Canvas.drawBLDSNode(i : Int, scale : Float, paint : Paint) {
     drawBiLineDots(scale, w, paint)
     restore()
 }
+
+class BiLineDotBouncyStepView(ctx : Context) : View(ctx) {
+
+    private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    override fun onDraw(canvas : Canvas) {
+
+    }
+
+    override fun onTouchEvent(event : MotionEvent) : Boolean {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+
+            }
+        }
+        return true
+    }
+}
