05aadac9af5eeb462c83d43cb8892b796ae8492b
Anwesh43 <anweshthecool0@gmail.com>
2020-01-31 10:49:42 +0530

    adding constants and libraries

--- bilinedotbouncystepview/src/main/java/com/anwesh/uiprojects/bilinedotbouncystepview/BiLineDotBouncyStepView.kt
+++ bilinedotbouncystepview/src/main/java/com/anwesh/uiprojects/bilinedotbouncystepview/BiLineDotBouncyStepView.kt
@@ -3,3 +3,20 @@ package com.anwesh.uiprojects.bilinedotbouncystepview
 /**
  * Created by anweshmishra on 31/01/20.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val lines : Int = 2
+val rFactor : Float = 6f
+val scGap : Float = 0.02f
+val strokeFactor : Float = 90f
+val delay : Long = 20
+val foreColor : Int = Color.parseColor("#283593")
+val backColor : Int = Color.parseColor("#BDBDBD")
